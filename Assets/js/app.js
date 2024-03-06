39d268b75b13ad07063d7d1cec6d24ba7d927caf
kecskeszsoltistvan <78853814+kecskeszsoltistvan@users.noreply.github.com>
2024-03-06 10:28:52 +0100

    Kiválasztható termék

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,9 +1,34 @@
 const serverURL = 'http://localhost:5000';
 let kategoria_select = document.querySelector('#inputGroupSelect01');
+let termek_select = document.querySelector('#inputGroupSelect02');
+let egyseg_price = document.querySelector('#egyseg_ar');
 
 kategoria_select.addEventListener("change", (event) => {
     console.log(kategoria_select.value);
+    axios.get(`${serverURL}/items/cat/"${kategoria_select.value}"`).then(res=>{
+        termek_select.innerHTML = ''
+        res.data.forEach(item =>{
+            console.log(item.nev);
+            let opcio = document.createElement('option');
+            opcio.value = item.nev;
+            opcio.text = `${item.nev}`;
+            termek_select.appendChild(opcio);
+        })
+    })
 });
+
+termek_select.addEventListener("change", (event) => {
+    console.log(termek_select.value);
+    axios.get(`${serverURL}/items/item/"${termek_select.value}"`).then(res=>{
+        res.data.forEach(item =>{
+            egyseg_price.ariaDisabled = false;
+            egyseg_price.value = `${item.ar}`;
+            egyseg_price.ariaDisabled = true;
+        })
+    })
+});
+
+
 function fill_list(){
     setTimeout(()=>{
 
