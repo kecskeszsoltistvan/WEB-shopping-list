dd103b24e005e4ab6a85618553b8067f39f06d8d
kecskeszsoltistvan <kecskeszsoltistvan@turr.hu>
2024-03-10 16:40:26 +0100

    Javítás + mentés kivitelezése

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -6,6 +6,64 @@ let amount = document.querySelector('#mennyi');
 let osszar = document.querySelector('#osszar');
 let table = document.querySelector('#asztal');
 let gonb = document.querySelector('#gonb');
+let mentes = document.querySelector('#gonb2');
+let torles = document.querySelector('#gonb3');
+let nemtom = document.querySelector('#gonb4');
+let osszar_szamolo = document.querySelector('#osszar_szamolo');
+
+function getColumn(col) {
+    let tab = table;
+    let n = tab.rows.length;
+    let i = 0;
+    let s = 0;
+    if (col < 0) {
+        return null;
+    }
+
+    for (i = 1; i < n; i++) {
+        tr = tab.rows[i];
+        if (tr.cells.length > col) { 
+            td = tr.cells[col];
+            s += Number(td.innerText);
+        } 
+    }
+    return s;
+}
+
+function osszar_szamolas(){
+    osszar_szamolo.innerHTML = `Összár: ${getColumn(3)}ft`;
+}
+
+table.querySelectorAll('button').forEach(btn => btn.addEventListener("click", function(e){
+    this.parentNode.remove();
+}));
+
+if (localStorage.getItem("Tartalom") != null){
+    table.innerHTML = localStorage.getItem("Tartalom");
+    osszar_szamolas();
+}
+
+mentes.addEventListener("click", (event) => {
+    localStorage.setItem("Tartalom", table.innerHTML);
+    osszar_szamolas()
+})
+
+torles.addEventListener("click", (event) => {
+    if (confirm("Biztosan kitörli a jelenlegi lista tartalmát?"))
+    {
+        localStorage.removeItem("Tartalom");
+        table.innerHTML = `<thead id="asztal">
+        <tr>
+          <th scope="col">Terméknév</th>
+          <th scope="col">Mennyiség</th>
+          <th scope="col">Egységár</th>
+          <th scope="col">Összesen</th>
+          <th scope="col">Műveletek</th>
+        </tr>
+      </thead>`;
+    }
+
+})
 
 kategoria_select.addEventListener("change", (event) => {
     axios.get(`${serverURL}/items/cat/"${kategoria_select.value}"`).then(res=>{
@@ -16,6 +74,16 @@ kategoria_select.addEventListener("change", (event) => {
             opcio.text = `${item.nev}`;
             termek_select.appendChild(opcio);
         })
+        termek_select.selectedIndex = 0;
+        axios.get(`${serverURL}/items/item/"${termek_select[termek_select.selectedIndex].value}"`).then(res=>{
+            res.data.forEach(targy => {
+                egyseg_price.ariaDisabled = false;
+                egyseg_price.value = `${targy.ar}`;
+                egyseg_price.ariaDisabled = true;
+                osszar.value = amount.value * targy.ar;
+                
+            })
+        })
     })
 });
 
@@ -25,6 +93,7 @@ termek_select.addEventListener("change", (event) => {
             egyseg_price.ariaDisabled = false;
             egyseg_price.value = `${item.ar}`;
             egyseg_price.ariaDisabled = true;
+            osszar.value = amount.value * item.ar;
         })
     })
 });
@@ -49,25 +118,34 @@ function fill_list(){
         })
     }, 500);
 }
-gonb.addEventListener("click", (event) => { 
-    let sor = document.createElement('tr');
-    let adat1 = document.createElement('th');
-    let adat2 = document.createElement('th');
-    let adat3 = document.createElement('th');
-    let adat4 = document.createElement('th');
-    let adat5 = document.createElement('th');
-    adat1.innerHTML = termek_select.value;
-    adat2.innerHTML = amount.value;
-    adat3.innerHTML = egyseg_price.value;
-    adat4.innerHTML = osszar.value;
-    adat5.innerHTML = "";
-    sor.appendChild(adat1);
-    sor.appendChild(adat2);
-    sor.appendChild(adat3);
-    sor.appendChild(adat4);
-    sor.appendChild(adat5);
-    console.log("H");
-    table.appendChild(sor);
 
+gonb.addEventListener("click", (event) => { 
+    if (amount.value != "" && egyseg_price.value != "" && osszar.value != "" || amount.value < 1){
+        let sor = document.createElement('tr');
+        let adat1 = document.createElement('th');
+        let adat2 = document.createElement('th');
+        let adat3 = document.createElement('th');
+        let adat4 = document.createElement('th');
+        let adat5 = document.createElement('th');
+        adat1.innerHTML = termek_select.value;
+        adat2.innerHTML = amount.value;
+        adat3.innerHTML = egyseg_price.value;
+        adat4.innerHTML = osszar.value;
+        let t_g = document.createElement('button');
+        t_g.setAttribute("class", "delete");
+        t_g.innerHTML = "X";
+        t_g.setAttribute("onClick", "osszar_szamolas(); return this.parentNode.parentNode.remove();")
+        adat5.appendChild(t_g)
+        sor.appendChild(adat1);
+        sor.appendChild(adat2);
+        sor.appendChild(adat3);
+        sor.appendChild(adat4);
+        sor.appendChild(adat5);
+        table.appendChild(sor);
+    }
+    else{
+        alert("Nincs meg minden adat vagy rosszul.");
+    }
+    osszar_szamolas()
 });
 fill_list();
